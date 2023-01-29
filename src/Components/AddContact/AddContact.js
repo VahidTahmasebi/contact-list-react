649d122066d67ae0f16c185f383c88033ca9407e
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-01-29 13:01:14 +0330

    Send data to the server

--- src/Components/AddContact/AddContact.js
+++ src/Components/AddContact/AddContact.js
@@ -1,20 +1,34 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import addOneContact from "../../services/addContactService";
 
 const AddContact = () => {
   const [contact, setContact] = useState({ name: "", email: "" });
 
+  const navigate = useNavigate();
+
+  // change handler for input all
   const changeHandler = (e) => {
     setContact({ ...contact, [e.target.name]: e.target.value });
   };
 
-  const submitForm = (e) => {
+  // data send the form handler
+  const submitForm = async (e) => {
     // if the from values are empty, display a message
     if (!contact.name || !contact.email) {
       alert("all fields are mandatory");
       return;
     }
-    setContact({ name: "", email: "" });
     e.preventDefault();
+    try {
+      // send data to the server
+      await addOneContact(contact);
+      setContact({ name: "", email: "" });
+      // after clicking, it will transfer to the main page
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
